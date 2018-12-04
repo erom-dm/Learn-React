d5cc4f0379ab00313b70e38250274c8e8c9f4ed9
erom-dm <swiftyerom@gmail.com>
2018-12-04 23:38:21 +0200

    implemented battle, added loading component

--- app/index.js
+++ app/index.js
@@ -1,21 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
 require('./index.css');
-
 var App = require('./components/App');
-
-// example for PropTypes usage
-/*Badge.propTypes = {
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    // requires an array of objects, that correspond to following requierments.
-    list: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        age: PropTypes.number.isRequired
-    }))
-}*/
 
 ReactDOM.render(
     <App/>,
